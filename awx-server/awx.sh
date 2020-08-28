32826ee2677f13f4c6993b708e368697e1f26057
conor johnston <cnrjohnston@gmail.com>
2020-08-28 17:17:18 +0100

    removed newgrp

--- awx-server/awx.sh
+++ awx-server/awx.sh
@@ -32,7 +32,6 @@ sudo su -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key ad
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update && sudo apt install docker-ce -y
 sudo usermod -aG docker $USER
-newgrp docker
 
 echo "------------------"
 echo "Installing Docker Compose"
